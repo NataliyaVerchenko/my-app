ed5e16863c1b8fdfbfcd9a6141c824219220904e
НАТАЛИ ВЕРЧЕНКО <ver4.nata@gmail.com>
2018-10-31 12:59:42 +0200

    Change file App.js and index.js components for my-app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
-import './App.css';
-import Page from './components/Page';
-import Header from "./components/Header";
+
+import { Users, Photos, Todos, Comments, PostSingle, Albums, Posts} from './modules';
+
+import { Route } from "react-router-dom";
+
 
 class App extends Component {
     render() {
         return (
-            <div>
-              <Header/>
-              <Page/>
+            <div className="App">
+                <Route exact path="/" component={Posts} />
+                <Route exact path="/posts" component={Posts} />
+                <Route path="/users" component={Users} />
+                <Route path="/albums" component={Albums} />
+                <Route path="/photos" component={Photos} />
+                <Route path="/todos" component={Todos} />
+                <Route path="/comments" component={Comments} />
+                <Route path="/posts/:id" component={PostSingle} />
+
             </div>
         );
     }
 }
+
 export default App;
